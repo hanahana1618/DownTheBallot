b3986c36f0db401500c2b603a827a760a3938af4
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2016-10-01 07:44:14 -0400

    deleted info_c

--- html/info_c.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    // configuration
-    require(__DIR__ . "/includes/config.php");
-
-    // if user reached page via GET (as by clicking a link or via redirect)
-    render("info.html", ["title" => "Down The Ballot | Information"]);
-
-?>
